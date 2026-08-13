3786524852813e52f72a8cbce7c6df41a46a35e4
dhruviltejani <tejanidhruvilofficial@gmail.com>
2026-08-13 18:16:06 +0530

    Made .env.example file and removed some non wanted files .

--- Backend/password.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-async function hashPassword() {
-  const hashedPassword = await bcrypt.hash("tejani1234", 10);
-  console.log(hashedPassword);
-}
-
-hashPassword();
